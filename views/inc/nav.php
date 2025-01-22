eb53a5bd537a6c736653c43a90fe6fe178f0b812
anilexs <alexissimoes94@gmail.com>
2025-01-22 13:21:36 +0100

    nav logo 1

--- views/inc/nav.php
+++ views/inc/nav.php
@@ -7,6 +7,18 @@ if(isset($_COOKIE[''])){ ?>
     </nav>
 <?php }else{ ?>
     <nav>
-        
+        <ul>
+            <li class="logoContener">
+                <a href="<?= $host ?>">
+                    <img src="<?= $host ?>asset/img/logo2.png" alt="logo img" class="logo">
+                </a>
+            </li>
+            
+            <li class="logoa">
+                <a href="<?= $host ?>">
+                    
+                </a>
+            </li>
+        </ul>
     </nav>
 <?php } ?>
