3a0857b121b88c08bcc71f0968ce15ccbbb55a60
anilexs <alexissimoes94@gmail.com>
2025-01-25 23:58:56 +0100

    menu animation 2

--- views/asset/js/nav.js
+++ views/asset/js/nav.js
@@ -12,25 +12,13 @@ $(document).ready(function(){
             $(selecteur).stop(true, true).animate({
                 maxHeight: contentHeight
             }, 100);
-            $('.langSelect').css({
-                'border-bottom-right-radius': '0px',
-                'border-bottom-left-radius': '0px'
-            });            
 
             $(selecteur).removeClass('close');
         } else {
             
             $(selecteur).stop(true, true).animate({
                 maxHeight: 0
-            }, 100, function () {
-                setTimeout(function () {
-                    $('.langSelect').css({
-                        'border-bottom-right-radius': '10px',
-                        'border-bottom-left-radius': '10px'
-                    });       
-                }, 300);
-
-            });
+            }, 100);
     
             $(selecteur).addClass('close');
         }
