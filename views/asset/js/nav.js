5f1b1497487c098df801d62337fb298ae6155cdb
anilexs <alexissimoes94@gmail.com>
2025-01-25 23:46:01 +0100

    menu1

--- views/asset/js/nav.js
+++ views/asset/js/nav.js
@@ -12,13 +12,25 @@ $(document).ready(function(){
             $(selecteur).stop(true, true).animate({
                 maxHeight: contentHeight
             }, 100);
+            $('.langSelect').css({
+                'border-bottom-right-radius': '0px',
+                'border-bottom-left-radius': '0px'
+            });            
 
             $(selecteur).removeClass('close');
         } else {
             
             $(selecteur).stop(true, true).animate({
                 maxHeight: 0
-            }, 100);
+            }, 100, function () {
+                setTimeout(function () {
+                    $('.langSelect').css({
+                        'border-bottom-right-radius': '10px',
+                        'border-bottom-left-radius': '10px'
+                    });       
+                }, 300);
+
+            });
     
             $(selecteur).addClass('close');
         }
